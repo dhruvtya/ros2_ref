fc871f7802a3e59c1c9043149da9bfe46dd2eeab
Dhruv Tyagi <dhruv.tya@gmail.com>
2022-06-11 01:01:36 +0530

    Added config comments to launch

--- ex_launch_pkg/launch/ex_launch_1.launch.py
+++ ex_launch_pkg/launch/ex_launch_1.launch.py
@@ -1,9 +1,18 @@
+# import os
+# from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     ld = LaunchDescription()
+
+    # config_file = os.path.join(
+    #   get_package_share_directory('package_name'),
+    #   'config',
+    #   'config_file_name.yaml'
+    # )
 
     ex_cpp_pub_server_node = Node(
         package="example_cpp_pkg",
@@ -15,6 +24,8 @@ def generate_launch_description():
         parameters=[
             {"ex_param_2": "Different string"}
         ]
+        # namespace="can_put_namespace"
+        # parameters=[config_file]
     )
     ex_cpp_sub_client_node = Node(
         package="example_cpp_pkg",
